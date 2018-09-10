2b6e4c93a6ada0b575f5b941d72df1c071f6afde
Han <tranthithuhan@gmail.com>
2018-09-10 18:35:02 +0200

    test mine block

--- main.js
+++ main.js
@@ -7,16 +7,27 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = '';
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce ++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("block mined: ", this.hash);
     }
 }
 
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
     }
 
     createGenesisBlock() {
@@ -29,7 +40,7 @@ class Blockchain {
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLastestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -49,8 +60,13 @@ class Blockchain {
 }
 
 let testCoin = new Blockchain();
+
+
+console.log("mining block 1 ...");
 testCoin.addBlock(new Block(1, "11/09/2018", {amount: 4}));
+
+console.log("mining block 2 ...");
 testCoin.addBlock(new Block(1, "12/09/2018", {amount: 5}));
 
-console.log(testCoin.isChainValide());
-console.log(JSON.stringify(testCoin, null, 4));
+//console.log(testCoin.isChainValide());
+// console.log(JSON.stringify(testCoin, null, 4));
